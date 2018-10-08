06a1d1dc32ac05dcce4b2ffe460799df7a0d914a
jiangconghu01 <1239395043@qq.com>
2018-10-08 11:50:21 +0800

    formatter

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,31 +1,31 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const webpack = require('webpack');
+const path = require('path')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const webpack = require('webpack')
 module.exports = merge(common, {
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, '../dist'),
-        index: 'czxt.html',
-        compress: true,
-        hot: true,
-        host: '0.0.0.0',
-        //port: 9000,
-        port: 7001,
-        proxy: {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, '../dist'),
+    index: 'czxt.html',
+    compress: true,
+    hot: true,
+    host: '0.0.0.0',
+    // port: 9000,
+    port: 7001,
+    proxy: {
 
-            '/czxt_war/*': {
-                //target: 'http://134.96.245.118:7001',
-                target: 'http://134.96.188.185:9000',
-                //changeOrigin: true,
-                secure: false,
-                // auth: 'admin': '1q2w3e4r'
-            }
-        }
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ]
-});
+      '/czxt_war/*': {
+        // target: 'http://134.96.245.118:7001',
+        target: 'http://134.96.188.185:9000',
+        // changeOrigin: true,
+        secure: false
+        // auth: 'admin': '1q2w3e4r'
+      }
+    }
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+})
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,21 +1,21 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 module.exports = merge(common, {
-    mode: 'production',
-    plugins: [
-        //new UglifyJSPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[contenthash][name].style.css'
-        }),
-        new OptimizeCSSAssetsPlugin({
-            assetNameRegExp: /\.style\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: { safe: true, discardComments: { removeAll: true } },
-            canPrint: true
-        })
-    ],
+  mode: 'production',
+  plugins: [
+    // new UglifyJSPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[contenthash][name].style.css'
+    }),
+    new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.style\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { safe: true, discardComments: { removeAll: true } },
+      canPrint: true
+    })
+  ]
 
-});
+})
